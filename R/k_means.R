41a949033356e74ecba2da82b2bd006a4ac1b6f5
emmanuelparadis <Emmanuel.Paradis@ird.fr>
2021-09-03 11:14:20 +0700

    removed LICENCE file

--- R/k_means.R
+++ R/k_means.R
@@ -9,6 +9,7 @@
 
 pkm <- function(x, cls, K, threshold = 1e-5, iter.lim = 200, quiet = FALSE)
 {
+    if (is.vector(x)) x <- as.matrix(x)
     if (length(cls) != nrow(x))
         stop("number of values in 'cls' not equal to numeber of rows in 'x'")
     PARA <- as.integer(c(K, threshold * length(cls), iter.lim, quiet))
